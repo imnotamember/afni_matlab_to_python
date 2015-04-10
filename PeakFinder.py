e0890b4580051cb70c82b798ce4e7640d7c23bce
Joshua Zosky <joshua.e.zosky@gmail.com>
2015-04-10 16:26:08 -0500

    Updated and Restructured workspace. Progress made in Defaults and file operations.

---
+++ PeakFinder.py
@@ -0,0 +1,14 @@
+__author__ = 'Joshua Zosky'
+import sys
+
+'''
+
+for item in sys.argv[1:7]:
+    try:
+        retroTS_Dictionary[(item.split('='))[0]] = int(item.split('=')[1])
+    except:
+        retroTS_Dictionary[(item.split('='))[0]] = item.split('=')[1]
+for item in retroTS_Dictionary:
+    print "%s:%s" % (item,retroTS_Dictionary[item])
+
+'''
